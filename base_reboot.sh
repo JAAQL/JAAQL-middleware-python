65b545aa41e6e83e391917cfbe67a35a8059d801
aarontasker <aaron.tasker@gmail.com>
2021-11-29 22:15:00 +0100

    Added logging

--- base_reboot.sh
+++ base_reboot.sh
@@ -6,8 +6,13 @@ echo "Running base reboot script"
 if [ -f "/JAAQL-middleware-python/redeploy" ] ; then
     echo "Redeploying"
     git clone https://github.com/JAAQL/JAAQL-middleware-python.git /JAAQL-middleware-python/JAAQL-middleware-python
+    echo "Finished clone"
     rm -rf /JAAQL-middleware-python/JAAQL-middleware-python/.git
+    echo "Removed git folder"
     cp -rpf /JAAQL-middleware-python/JAAQL-middleware-python/* /JAAQL-middleware-python
+    echo "Overwrote files"
     rm -rf /JAAQL-middleware-python/JAAQL-middleware-python
+    echo "Deleted copy"
     rm -rf /JAAQL-middleware-python/redeploy
+    echo "Finished redeploy"
 fi
