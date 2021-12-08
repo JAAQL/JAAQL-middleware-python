ca05eea276cb841f3cd51f3ac61ffcf7402c410e
aarontasker <aaron.tasker@gmail.com>
2021-12-08 10:35:27 +0100

    Fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -95,10 +95,9 @@ CERT_DIR=/etc/letsencrypt/live/$SERVER_ADDRESS
 if [[ "$IS_HTTPS" = "TRUE" && ! -d "$CERT_DIR" ]] ; then
   echo "Initialising certbot"
   /pypy3.7-v7.3.5-linux64/bin/certbot --nginx -d $SERVER_ADDRESS -d www.$SERVER_ADDRESS --redirect --noninteractive --no-eff-email --email $HTTPS_EMAIL --agree-tos -w $INSTALL_PATH/www
-fi
-if [[ "$IS_HTTPS" = "TRUE" && -d "$CERT_DIR" ]] ; then
+elif [[ "$IS_HTTPS" = "TRUE" && -d "$CERT_DIR" ]] ; then
   echo "Found existing certificates. Installing"
-  /pypy3.7-v7.3.5-linux64/bin/certbot --nginx
+  printf "1\n" | /pypy3.7-v7.3.5-linux64/bin/certbot --nginx
 fi
 
 rm -rf /etc/nginx/sites-enabled/default
