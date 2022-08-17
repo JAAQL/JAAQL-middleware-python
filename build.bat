6835476470c4e9bea8b65885a989cb780633e6e3
Aaron Tasker <45070792+aarontasker@users.noreply.github.com>
2022-08-17 19:13:22 +0200

    Version 3.1.21

---
+++ build.bat
@@ -0,0 +1,14 @@
+@echo off
+ECHO This script will build jaaql and push to pypi and docker
+python setup.py sdist bdist_wheel
+python version.py>version.txt
+set /p VERSION=<version.txt
+del version.txt
+twine upload dist/*
+rmdir build /s /q
+rmdir dist /s /q
+rmdir jaaql_middleware_python.egg-info /s /q
+docker build -t jaaql/jaaql-middleware-python -f docker/Dockerfile .
+docker tag jaaql/jaaql-middleware-python jaaql/jaaql-middleware-python:%VERSION%
+docker push jaaql/jaaql-middleware-python
+docker push jaaql/jaaql-middleware-python:%VERSION%
---
+++ version.py
@@ -0,0 +1,4 @@
+from jaaql.constants import VERSION
+
+if __name__ == '__main__':
+    print(VERSION)
