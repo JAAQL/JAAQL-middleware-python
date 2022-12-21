15194cb8936a84fb8f4a029ba37593dfe844b269
Aaron Tasker <aaron.tasker@gmail.com>
2022-12-21 10:45:39 +0100

    Version 4.1.6

--- docker/pg_reload.sh
+++ docker/pg_reload.sh
@@ -1,2 +1,2 @@
-sed -i 's/trust/md5/g' /var/lib/postgresql/data/pg_hba.conf
+# sed -i 's/trust/md5/g' /var/lib/postgresql/data/pg_hba.conf
 ./usr/lib/postgresql/15/bin/pg_ctl reload -D /var/lib/postgresql/data
