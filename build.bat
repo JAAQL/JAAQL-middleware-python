86b421980696f0bbd86e6d1b533a8442b286c6cf
Aaron Tasker <aaron.tasker@gmail.com>
2023-01-30 17:30:54 +0100

    Improvements to build

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 ECHO This script will build jaaql and push to pypi and docker
-python setup.py sdist bdist_wheel
+REM python setup.py sdist bdist_wheel
 python version.py>version.txt
 set /p VERSION=<version.txt
 del version.txt
-py -m twine upload dist/*
+REM py -m twine upload dist/*
 rmdir build /s /q
 rmdir dist /s /q
 rmdir jaaql_middleware_python.egg-info /s /q
