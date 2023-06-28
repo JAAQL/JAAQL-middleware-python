507bcd9c99ff0b78152ea59790df85e893720cc6
Aaron Tasker <aaron.tasker@gmail.com>
2023-06-28 21:19:57 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @echo off
 ECHO This script will build jaaql and push to pypi and docker
 python setup.py sdist bdist_wheel
+py -m twine upload dist/*
 python version.py>version.txt
 set /p VERSION=<version.txt
 del version.txt
@@ -11,4 +12,3 @@ docker build -t jaaql/jaaql-middleware-python -f docker/Dockerfile .
 docker tag jaaql/jaaql-middleware-python jaaql/jaaql-middleware-python:%VERSION%
 docker push jaaql/jaaql-middleware-python
 docker push jaaql/jaaql-middleware-python:%VERSION%
-py -m twine upload dist/*
