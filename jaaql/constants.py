59e8186a621cc6ce4ff63f560e49d6e6a5b16247
Aaron Tasker <aaron.tasker@gmail.com>
2023-11-08 01:14:56 +0100

    Fix

--- jaaql/constants.py
+++ jaaql/constants.py
@@ -147,5 +147,5 @@ ROLE__postgres = "postgres"
 
 PROTOCOL__postgres = "postgresql://"
 
-VERSION = "4.21.14"
+VERSION = "4.21.16"
 
--- jaaql/email/email_manager_service.py
+++ jaaql/email/email_manager_service.py
@@ -118,7 +118,7 @@ class DrivenChrome:
                     break
                 else:
                     if time_delta_ms(start_time, datetime.now()) > TIMEOUT__attachment_render:
-                        print(self.driver.page_source)
+                        print(self.driver.find_element(By.TAG_NAME, "body").get_attribute("outerHTML"))
                         raise HttpStatusException(ERR__attachment_timeout_render)
 
             return filename, base64.b64decode(self.driver.execute_cdp_cmd("Page.printToPDF", self.a4_params)["data"])
