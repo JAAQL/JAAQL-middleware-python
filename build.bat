c49479ad03dcb715211a9529956b37eb3e57b6fb
Aaron Tasker <aaron.tasker@gmail.com>
2025-04-16 14:58:34 +0200

    Prog save

--- build.bat
+++ build.bat
@@ -8,6 +8,5 @@ py -m twine upload dist/*
 rmdir build /s /q
 rmdir dist /s /q
 rmdir jaaql_middleware_python.egg-info /s /q
-docker build -t jaaql/jaaql-middleware-python -f docker/Dockerfile .
-docker tag jaaql/jaaql-middleware-python jaaql/jaaql-middleware-python:%VERSION%
+docker build -t jaaql/jaaql-middleware-python:%VERSION% -f docker/Dockerfile .
 docker push jaaql/jaaql-middleware-python:%VERSION%
